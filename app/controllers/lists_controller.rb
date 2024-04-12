53e19fd33c4055945e0817cacb340c126032156d
ikedakoichiro13 <164018837+ikedakoichiro13@users.noreply.github.com>
2024-04-12 15:08:33 +0000

    app-4

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,9 +1,9 @@
 class ListsController < ApplicationController
-  
+
   def new
     @list = List.new
   end
-  
+
   def create
     list = List.new(list_params)
     list.save
@@ -11,14 +11,16 @@ class ListsController < ApplicationController
   end
 
   def index
+    @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
   end
-  
+
   private
   def list_params
     params.require(:list).permit(:title, :body)
